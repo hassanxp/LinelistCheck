8e34d241e07e1037452366b3fcf7ede2f59d3a1c
Hassan Siddiqui <hassans@astro.princeton.edu>
2022-04-04 11:21:23 -0400

    committed older stuff

--- convertAirLineList.py
+++ convertAirLineList.py
@@ -5,8 +5,10 @@ def main():
     filename = 'osterbrock1996_order_44_45.csv'
     print(f'Reading input file {filename}...')
     outfile = f'{filename}-vac'
-    generateAirLineList(filename, outfile, lambdaNm=False, erin=False)
-    print(f'Output written to {outfile}')
+    infoFile = f'{filename}-info.csv'
+    generateAirLineList(filename, outfile, infoFile,
+                        lambdaNm=False, erin=False)
+    print(f'Output written to {outfile}. Conversion table in {infoFile}')
 
 
 if __name__ == "__main__":

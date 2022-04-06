22e764f4d9993272ecbc28589b0871dc046ed34c
Hassan Siddiqui <hassans@astro.princeton.edu>
2022-04-05 20:48:07 -0400

    added code to merge osterbrock and rousselot lists

---
+++ convertRousselot.py
@@ -0,0 +1,44 @@
+import os
+from utils import referenceLineSetToDataFrame
+from pfs.drp.stella.referenceLine import ReferenceLineSet, ReferenceLine
+from pfs.drp.stella.referenceLine import ReferenceLineStatus, ReferenceLineSource
+
+
+def convert(filename: str, outfile: str, infofile: str):
+    with open(filename, 'r') as f:
+        linelist = []
+        for line in f:
+            lineStrp = line.strip()
+            if lineStrp.startswith('#'):
+                continue
+            fields = lineStrp.split()
+            waveVacAngs, intensity = fields
+            trans = "UNKNOWN"
+            waveVacNm = float(waveVacAngs)/10.
+            refLine = ReferenceLine(description='OH',
+                                    wavelength=waveVacNm,
+                                    intensity=float(intensity),
+                                    status=ReferenceLineStatus.GOOD,
+                                    transition=trans,
+                                    source=ReferenceLineSource.ROUSSELOT2000)
+            linelist.append(refLine)
+
+        lineListSorted = sorted(linelist,
+                                key=lambda refLine: refLine.wavelength)
+
+        df = referenceLineSetToDataFrame(lineListSorted)
+        rls = ReferenceLineSet(df)
+        rls.writeLineList(outfile)
+
+
+def main():
+    filename = 'Rousellot_list_v2.0.dat'
+    fullfilename = os.path.join('data', 'Rousselot+2000', filename)
+    print(f'Reading input file {fullfilename}...')
+    outfile = f'{filename}-out'
+    infofile = f'{filename}-info'
+    convert(fullfilename, outfile, infofile)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mergeOsterBrockRousselot.py
@@ -0,0 +1,67 @@
+import os
+from utils import toVacuum, referenceLineSetToDataFrame
+from pfs.drp.stella.referenceLine import ReferenceLineSet, ReferenceLine
+from pfs.drp.stella.referenceLine import ReferenceLineStatus, ReferenceLineSource
+
+
+def merge(rouFilename: str, ostFilename: str, outfile: str):
+    rouRLS = ReferenceLineSet.fromLineList(rouFilename)
+    ostRLS = ReferenceLineSet.fromLineList(ostFilename)
+
+    mergedList = []
+
+    rouRLS.sort()
+    ostRLS.sort()
+
+    rouIdx = 0
+    ostIdx = 0
+    while rouIdx < len(rouRLS) and ostIdx < len(ostRLS):
+        rouRL = rouRLS[rouIdx]
+        ostRL = ostRLS[ostIdx]
+        print(f'Looking at Ost wavelength {ostRL.wavelength} and Rou wavelength {rouRL.wavelength}..')
+
+        if abs(ostRL.wavelength - rouRL.wavelength) < 0.0001:
+            print(f'    merging line w. Ost wavelength {ostRL.wavelength} and Rou wavelength {rouRL.wavelength})')
+            rouRL.transition = ostRL.transition
+            rouRL.source = ReferenceLineSource.ROUSSELOT2000EXT
+            mergedList.append(rouRL)
+            ostIdx += 1
+            rouIdx += 1
+            continue
+
+        if ostRL.wavelength < rouRL.wavelength:
+            mergedList.append(ostRL)
+            ostIdx += 1
+            continue
+
+        mergedList.append(rouRL)
+        rouIdx += 1
+
+    # Complete list with remaining lines
+    # from either outstanding list
+    if rouIdx < len(rouRLS):
+        while rouIdx < len(rouRLS):
+            rouRL = rouRLS[rouIdx]
+            mergedList.append(rouRL)
+            rouIdx += 1
+    else:
+        if ostIdx < len(ostRLS):
+            while ostIdx < len(ostRLS):
+                ostRL = ostRLS[ostIdx]
+                mergedList.append(ostRL)
+                ostIdx += 1
+
+
+    mergedLineSet = ReferenceLineSet.fromRows(mergedList)
+    mergedLineSet.writeLineList(outfile)
+
+
+def main():
+    rouFilename = 'Rousellot_list_v2.0.dat-out'
+    ostFilename = 'oh.dat-out'
+    outfile = f'{rouFilename}-merged'
+    merge(rouFilename, ostFilename, outfile)
+
+
+if __name__ == "__main__":
+    main()
